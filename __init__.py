20a27fa793a9018cdd10ce681759c208fbb6ae25
Jay Phillz <58239+jaysonjphillips@users.noreply.github.com>
2021-06-15 20:43:17 -0700

    barebones first voice skill

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from mycroft import MycroftSkill, intent_hander
+import redis
+import json
+
+def SmartMirrorNavigationSkill(MycroftSkill):
+    def __init__(self):
+        super(MycroftSkill, self).__init__("SmartMirrorNavigationSkill")
+        self.register_entity_file('location.entity')
+        self.redis_client = redis.Redis(host="192.168.1.11", port=6379, db=0)
+
+        @intent_hander('navigate.to.intent')
+        def handle_navigation_command(self, message):
+            destination = message.data.get('location')
+
+            if destination is not None:
+                publish_message = {
+                    'navigation': {
+                        'location': destination,    
+                    }
+                }
+
+                self.redis_client.publish(json.dumps(publish_message, separators=(',', ':')))
+    
+def create_skill(self):
+    return MycroftSkill()
